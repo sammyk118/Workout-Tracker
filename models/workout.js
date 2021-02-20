0d9c3beadbf2e591d610021da7188c6dfbf6f707
Sammy Kroner <sammyk118@gmail.com>
2021-02-19 23:21:20 -0800

    complete model

---
+++ models/workout.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+
+    exercises: [
+        {
+            type: {
+                type: String,
+                trim: true
+            },
+            name: {
+                type: String,
+                trim: true
+            },
+            distance: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            },
+            weight: {
+                type: Number
+            },
+            sets: {
+                type: Number
+            },
+            reps: {
+                type: Number
+            },
+            duration: {
+                type: Number
+            }
+        }
+    ]
+}
+)
+
+WorkoutSchema.virtual("totalDuration").get(function () {
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration;
+    }, 0);
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,4 +8,8 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"));
     })
+
+    app.get("stats", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"))
+    })
 }
